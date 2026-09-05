4a37bee6740f9ab0e064891117ff17ea3f22673e
kennedysenyo <kennedysenyo@gmail.com>
2026-09-05 17:00:02 +0000

    selection sort

---
+++ src/james_cutajar/chapt2/SelectionSort.java
@@ -0,0 +1,30 @@
+package james_cutajar.chapt2;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public int[] sort(int[] array ) {
+        int smallestIndex = 0;
+        for(int i = 0; i < array.length; i ++) {
+            smallestIndex = i;
+            for (int j = i; j < array.length; j ++) {
+                if (array[smallestIndex] > array[j]) {
+                    smallestIndex = j;
+                }
+            }
+            if(smallestIndex != i) {
+                int temp = array[i];
+                array[i] = array[smallestIndex];
+                array[smallestIndex] = temp;
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        SelectionSort selectionSort = new SelectionSort();
+        int[] array = new int[]{13,4,6,43,6,78,4,57,9,9};
+        System.out.println(Arrays.toString(selectionSort.sort(array)));
+    }
+}
